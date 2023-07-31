6ef9563296ba546d2b4f2aad7e48111fdd7ffde6
Reeves Smith <reeves.smith@outlook.com>
2023-07-31 06:49:49 -0600

    Update dim_dates.sql

--- models/marts/core/dim_dates.sql
+++ models/marts/core/dim_dates.sql
@@ -1,13 +1,10 @@
 WITH
 
 date_spine AS (
-
-{{ dbt_utils.date_spine(
-    datepart="day",
-    start_date="cast('2019-01-01' as date)",
-    end_date="cast('2030-01-01' as date)"
-   )
-}}
+    
+    SELECT DATEADD(DAY, SEQ4(), '2019-01-01')::DATE AS date_day
+    FROM TABLE(GENERATOR(ROWCOUNT => 365 * 10))  
+    
 ),
 
 final AS (
